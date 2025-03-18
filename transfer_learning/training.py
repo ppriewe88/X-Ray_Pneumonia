ad37d4d0c7d6633bcb86eec40ee84a6391dc0078
ppriewe88 <ppriewe@gmx.net>
2025-03-18 10:32:34 +0100

    initial sync

---
+++ transfer_learning/training.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import glob
+
