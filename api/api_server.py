9c5928d7ad55db7f2588e5aa088f9c3c0eddb3eb
ppriewe88 <ppriewe@gmx.net>
2025-04-04 16:19:23 +0200

    cleanup complete

--- api/api_server.py
+++ api/api_server.py
@@ -113,7 +113,7 @@ async def upload_image_with_label(
         logged_csv_data = ah.save_performance_data_csv(alias = alias, timestamp = api_timestamp, y_true = label.value, y_pred = y_pred, accuracy=accuracy_pred, file_name=file.filename, model_version=model_version, model_tag=model_tag)
 
         # switch off mlflow tracking (if needed)
-        mlflow_tracking = True 
+        mlflow_tracking = False 
         if mlflow_tracking:
             # logging in mlflow performance runs, if switched on
             ah.save_performance_data_mlflow(log_counter = logged_csv_data["log_counter"], alias = alias, timestamp = logged_csv_data["timestamp"], y_true = label, y_pred = y_pred, accuracy = accuracy_pred, file_name = logged_csv_data["filename"], model_version = model_version, model_tag = model_tag)
@@ -190,7 +190,7 @@ async def upload_image_and_integer_from_frontend(
         logged_csv_data = ah.save_performance_data_csv(alias = alias, timestamp = api_timestamp, y_true = label.value, y_pred = y_pred, accuracy=accuracy_pred, filename="123.jpeg", model_version=model_version, model_tag=model_tag)
 
         # switch off mlflow tracking (if needed)
-        mlflow_tracking = True 
+        mlflow_tracking = False 
         if mlflow_tracking:
             # logging in mlflow performance runs, if switched on
             ah.save_performance_data_mlflow(log_counter = logged_csv_data["log_counter"], alias = alias, timestamp = logged_csv_data["timestamp"], y_true = label, y_pred = y_pred, accuracy = accuracy_pred, file_name = logged_csv_data["filename"], model_version = model_version, model_tag = model_tag)
